121a3c459ebd83b31b9551a671827d15447d884a
kholood eassa <kholood.eassa@leannode.com>
2023-10-20 17:26:15 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,139 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Pressable, Text, ImageBackground, ScrollView } from "react-native";
+import axios from "axios";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const Stack = createNativeStackNavigator();
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 
-export default function App() {
+const CACHE_KEY = "REACT_QUERY_CACHE";
+const queryClient = new QueryClient();
+
+const Navigation = () => {
+  const { data, error, isLoading } = useQuery("restaurants", fetchRestaurants);
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    // we are retrieving data from storage before entering the screen for
+    //immediate show
+    loadQueryCache().then((res) => {
+      setRestaurants(res.businesses);
+    });
+
+    return () => {};
+  }, []);
+  useEffect(() => {
+    setRestaurants(data?.data.businesses);
+    return () => {};
+  }, [isLoading]);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Resturants"
+          component={(props) => (
+            <Resturants
+              {...props}
+              restaurants={restaurants}
+              isLoading={isLoading}
+            />
+          )}
+        />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Pressable
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+      onPress={() => navigation.navigate("Resturants")}
+    >
+      <Text style={{ backgroundColor: "yellow", padding: 5, borderRadius: 20 }}>
+        Go To Resturant
+      </Text>
+    </Pressable>
+  );
+};
+const Resturants = ({ navigation, restaurants, isLoading }) => {
+  useEffect(() => {
+    return () => {
+      // caching existing version of data on component unmount
+      const dataForRestaurants = queryClient.getQueryData("restaurants");
+      saveQueryCache(dataForRestaurants.data);
+    };
+  }, [navigation]);
+
+  return (
+    <ScrollView>
+      {restaurants?.map((rest) => (
+        <Pressable
+          onPress={() => navigation.navigate("Details", { restaurant: rest })}
+        >
+          <ImageBackground
+            style={{ width: 200, height: 200 }}
+            source={{
+              uri: rest.image_url,
+            }}
+          >
+            <Text style={{ color: "white" }}>{rest.name}</Text>
+          </ImageBackground>
+        </Pressable>
+      ))}
+    </ScrollView>
+  );
+};
+const Details = ({ navigation, route }) => {
+  const { restaurant } = { ...route.params };
+  return <Text>{JSON.stringify(restaurant)}</Text>;
+};
+const fetchRestaurants = () => {
+  return axios
+    .get(
+      "https://raw.githubusercontent.com/kholood-ea/inova-assignment/master/Resturants.json"
+    )
+    .then((response) => {
+      return response;
+    });
+};
+
+export const saveQueryCache = async (cache) => {
+  await AsyncStorage.setItem(CACHE_KEY, JSON.stringify(cache));
+};
+
+export const loadQueryCache = async () => {
+  const cache = await AsyncStorage.getItem(CACHE_KEY);
+  return JSON.parse(cache);
+};
+export default function App() {
+  const [resturants, setRestaurants] = useState([]);
+  // react query shows stale data at first then fetches new data in the backgroiund
+  // which results showing instant data, also it provides automatic refetching on failure
+
+  // check if there is resturants stores
+  // loadQueryCache()
+  //  if (cachedData) {
+  //   queryClient.setQueryData(cachedData);
+  // }
+  // console.log(data);
+  useEffect(() => {
+    // return () => {
+    //   const currentCache = queryClient.getQueryCache();
+    //   saveQueryCache(currentCache);
+    // };
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Navigation />
+    </QueryClientProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
